5346f6da51f09ba155d642f3e97232865664b8f6
unknown <arkadiusz.micek@gmail.com>
2020-02-28 02:32:24 +0100

    Router, Header, Nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,35 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
 export default function Header() {
-  return <h1>hello from header</h1>;
+    return (
+        <header className="header">
+            <Link to="/">
+                <img src={logo} alt="logo" className="logo" />
+            </Link>
+            <nav>
+                <ul>
+                    <div>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/products">Products</Link>
+                        </li>
+                    </div>
+                    <div>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                        <li>
+                            <Link to="/cart">cart</Link>
+                        </li>
+                    </div>
+                </ul>
+            </nav>
+        </header>
+    );
 }
