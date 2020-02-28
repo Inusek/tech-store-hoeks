4eebb09a43998ec481d2a93a172d03f16957390d
unknown <arkadiusz.micek@gmail.com>
2020-02-28 02:47:40 +0100

    ver 0.02

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function About() {
-  return <h1>hello from about page</h1>;
+    return (
+        <section className="section about-section">
+            <h1 className="section-title">about us</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+                cumque numquam dolorem repellat necessitatibus ratione, atque
+                magni tempore quam reiciendis, labore itaque quia deserunt
+                perspiciatis dolor eius qui unde! Ipsam.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa,
+                architecto.
+            </p>
+        </section>
+    );
 }
