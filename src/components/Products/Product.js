d1af2ab1521d23a74d1af69e572591ebc7691ae0
unknown <arkadiusz.micek@gmail.com>
2020-02-28 06:03:58 +0100

    ver 0.03

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,5 +1,22 @@
 import React from "react";
-
-export default function Product() {
-  return <h1>hello from product</h1>;
+import { Link } from "react-router-dom";
+export default function Product({ id, title, price, image }) {
+    const url = image.url;
+    return (
+        <article className="product">
+            <div className="img-container">
+                <img src={url} alt={title} />
+                <Link
+                    to={`/products/${id}`}
+                    className="btn btn-primary product-link"
+                >
+                    Details
+                </Link>
+            </div>
+            <div className="product-footer">
+                <p className="product-title">{title}</p>
+                <p className="product-price">${price}</p>
+            </div>
+        </article>
+    );
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import ProductProvider from "./context/products";
+import { CartProvider } from "./context/cart";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <ProductProvider>
+        <CartProvider>
+            <App />
+        </CartProvider>
+    </ProductProvider>,
+    document.getElementById("root")
+);
